2ef3634047def85b52a1b051f72109ec9a1843b4
wizaqua <aquazero@outlook.jp>
2020-05-05 02:57:24 +0900

    input要素にイベントハンドラを追加

--- src/FormApp.js
+++ src/FormApp.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 export class FormApp extends Component {
+    handleInput() {
+        console.log('handleInput called!');
+    }
     send() {
         console.log('send called!');
     }
     render() {
         return (
             <div>
-                <input type="text" />
+                <input type="text" onChange={this.handleInput.bind(this)} />
                 <button onClick={this.send.bind(this)}>SEND</button>
             </div>
         );
